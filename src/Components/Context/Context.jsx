0937aaac2b745516340d209a3d9b16213c44fa77
Nisho <mdnisho0077@gmail.com>
2024-11-07 13:58:45 +0600

    Google gemini is added

---
+++ src/Components/Context/Context.jsx
@@ -0,0 +1,71 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { createContext, useState } from "react";
+import runChat from "../../config/gemini";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompt, setPrevPrompt] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setresultData] = useState("");
+  const delayPara = (index, nextword) => {
+    setTimeout(function () {
+      setresultData((prev) => prev + nextword);
+    }, 75 * index);
+  };
+
+  const onSent = async (prompt) => {
+    setresultData("");
+    setLoading(true);
+    setShowResult(true);
+    let response;
+    if (prompt !== undefined) {
+      response = await runChat(prompt);
+      setRecentPrompt(prompt);
+    } else {
+      setPrevPrompt((prev) => [...prev, input]);
+      setRecentPrompt(input);
+      response = await runChat(input);
+    }
+    let responseArray = response.split("**");
+    let newResponse = "";
+    for (let i = 0; i < responseArray.length; i++) {
+      if (i === 0 || i % 2 !== 1) {
+        newResponse += responseArray[i];
+      } else {
+        newResponse += "<b>" + responseArray[i] + "</b>";
+      }
+    }
+    let newResponse2 = newResponse.split("*").join("</br>");
+    let newResponseArray = newResponse2.split(" ");
+    for (let i = 0; i < newResponseArray.length; i++) {
+      const nextWord = newResponseArray[i];
+      delayPara(i, nextWord + " ");
+    }
+    setLoading(false);
+    setInput("");
+  };
+
+  const contextValue = {
+    prevPrompt,
+    setPrevPrompt,
+    onSent,
+    setRecentPrompt,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput,
+  };
+
+  return (
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+  );
+};
+
+export default ContextProvider;
